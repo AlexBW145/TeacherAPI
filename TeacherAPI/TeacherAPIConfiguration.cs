8f4c106b0f4a2ba5c7eda6ce712be790e1ed9c5b
AlexBW145 <92541723+AlexBW145@users.noreply.github.com>
2026-02-07 22:47:59 -0600

    Soooo...

--- TeacherAPI/TeacherAPIConfiguration.cs
+++ TeacherAPI/TeacherAPIConfiguration.cs
@@ -14,7 +14,7 @@ namespace TeacherAPI
         public static ConfigEntry<bool> EnableBaldi { get; internal set; }
         public static ConfigEntry<bool> EnableCustomWarningScreen  { get; internal set; }
         public static ConfigEntry<bool> DisableAssetsWarningScreen { get; internal set; }
-#if debug
+#if false
         public static ConfigEntry<bool> DebugMode { get; internal set; }
 #endif
         public static ConfigEntry<bool> DisableAssistingTeachers { get; internal set; }
